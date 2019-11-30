30fa6e41aeb5476e53df22e21f05e7f999edc7d5
hoangphuhung1998@gmail.com <hoangphuhung1998@gmail.com>
2019-11-30 04:47:27 -0800

    update character

--- Assets/Scripts/x.cs
+++ Assets/Scripts/x.cs
@@ -9,27 +9,71 @@ public class x : MonoBehaviour
     public Transform tf;
     public Rigidbody2D rb;
     public Animator animator;
-    bool jump = false;
-
+    public Transform firePoint;
+    Object bulletRef;
+    Object bulletRef2;
+    bool onGround = false;
+    void Start()
+    {
+	speed = Time.deltaTime;
+	bulletRef = Resources.Load("bullet");
+	bulletRef2 = Resources.Load("bullet2");
+    }
     // Update is called once per frame
     void Update()
     {
-        speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
-	animator.SetFloat("speed", Mathf.Abs(speed));
+	speed = Mathf.Abs(Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+	animator.SetFloat("speed", 100*speed);
 	tf.Translate(new Vector2(speed, 0));
+
 	if (Input.GetKey(KeyCode.LeftArrow)){
-            sr.flipX = true;
+            	tf.rotation = Quaternion.Euler(0, 180, 0);
         }
             
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            sr.flipX = false;
+            	tf.rotation = Quaternion.Euler(0, 0, 0);
         }      
-	if (Input.GetKeyDown(KeyCode.X))
+	if (Input.GetKeyDown(KeyCode.X) && onGround )
         {
-	    jump = true;
-            //rb.AddForce(new Vector2(0, 200));
+	    animator.SetBool("jump", true);
+            rb.AddForce(new Vector2(0, 200));
         }
+	if (Input.GetKeyDown(KeyCode.C))
+	{
+		animator.SetBool("shoot",true);
+		GameObject bullet = (GameObject)Instantiate(bulletRef, firePoint.position, firePoint.rotation);
+	}
+	if (Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.V))
+	{
+		animator.SetBool("shoot", false);
+	}
+	
+	if (Input.GetKeyDown(KeyCode.Z) && onGround)
+        {
+		if (tf.rotation.y == 0) rb.AddForce(new Vector2(150, 0));
+		else rb.AddForce(new Vector2(-150, 0));
+	    	animator.SetBool("dash", true);
+        }
+	if (Input.GetKeyUp(KeyCode.Z))
+		animator.SetBool("dash",false);
+	if (Input.GetKeyDown(KeyCode.V))
+        {
+		animator.SetBool("shoot",true);
+		GameObject bullet2 = (GameObject)Instantiate(bulletRef2, firePoint.position, firePoint.rotation);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "ground") {
+		animator.SetBool("jump",false);
+		onGround =true;
+	}
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if(col.gameObject.name=="ground")  
+            onGround = false;
     }
 
 }
